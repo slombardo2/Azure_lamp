46311b1191a4309872c7f8e4585782f7cf5e1597
slombardo2 <60935040+slombardo2@users.noreply.github.com>
2020-06-18 12:22:57 -0400

    Update lamp.tf

--- Azure/terraform/hcl/lamp/lamp.tf
+++ Azure/terraform/hcl/lamp/lamp.tf
@@ -238,7 +238,7 @@ resource "azurerm_managed_disk" "external" {
 resource "azurerm_virtual_machine_data_disk_attachment" "external" {
   count              = "${var.count}"
   managed_disk_id    = "${azurerm_managed_disk.external.*.id[count.index]}"
-  virtual_machine_id = "${var.name_prefix}-web-${random_id.default.hex}-vm"
+  virtual_machine_id = "${var.name_prefix}-${random_id.default.hex}-web-vm"
   lun                = "0"
   caching            = "ReadWrite"
 }
